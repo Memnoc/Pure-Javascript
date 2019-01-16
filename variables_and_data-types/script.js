cc76088e779cc28c900c8d528078d07bf5244890
Memnoc <smartdroidesign@gmail.com>
2019-01-16 14:06:16 +0000

    Variables' Mutation and Type Coercion

--- variables_and_data-types/script.js
+++ variables_and_data-types/script.js
@@ -19,3 +19,20 @@ console.log(job);
 /**********************
  * Variables' Mutation and Type Coercion
  */
+
+ var firstName = 'Matteo';
+ var age = 34;
+
+ // This is type coercion
+ console.log(firstName + ' ' + age);
+
+ var job, isMarried;
+
+ job = 'Teacher';
+ isMarried = false;
+
+ console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+ // Variable mutation
+ age = 'twenty eight';
+ console.log(age);
