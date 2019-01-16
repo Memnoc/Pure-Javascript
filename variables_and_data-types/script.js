d935eec68da2334cb90d1db4ecdb0fab6f1ffbc5
Memnoc <smartdroidesign@gmail.com>
2019-01-16 14:39:13 +0000

    Basic Operators

--- variables_and_data-types/script.js
+++ variables_and_data-types/script.js
@@ -1,6 +1,6 @@
 /**********************
  * Variables and Data Types
- */
+ ************************/
 
 var firstName = 'John';
 console.log(firstName);
@@ -18,21 +18,55 @@ console.log(job);
 
 /**********************
  * Variables' Mutation and Type Coercion
- */
+ ************************/
 
- var firstName = 'Matteo';
- var age = 34;
+var firstName = 'Matteo';
+var age = 34;
 
- // This is type coercion
- console.log(firstName + ' ' + age);
+// This is type coercion
+console.log(firstName + ' ' + age);
 
- var job, isMarried;
+var job, isMarried;
 
- job = 'Teacher';
- isMarried = false;
+job = 'Teacher';
+isMarried = false;
 
- console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
- // Variable mutation
- age = 'twenty eight';
- console.log(age);
+// Variable mutation
+age = 'twenty eight';
+console.log(age);
+
+/**********************
+* Basic Operators
+***********************/
+
+var now, yeahMark, yearJohn;
+now = 2019;
+ageJohn = 28;
+ageMark = 33;
+
+// Math Operators
+var yearJohn = now - 28;
+var yeahMark = now - 33;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now / 2);
+console.log(now * 2);
+console.log(now % 2);
+
+// Logical Operators
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
+
+
