ecf8f483bea06e4c5eb0fc95482f65bc74b1e76f
Memnoc <smartdroidesign@gmail.com>
2019-01-16 13:48:15 +0000

    variables and data types

---
+++ variables_and_data-types/script.js
@@ -0,0 +1,21 @@
+/**********************
+ * Variables and Data Types
+ */
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+console.log(lastName + " " + age);
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+job = 'Programmer';
+console.log(job);
+
+/**********************
+ * Variables' Mutation and Type Coercion
+ */
