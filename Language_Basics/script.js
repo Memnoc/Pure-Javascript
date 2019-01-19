01442a3fab456ca4131bf884ecd8a3628d8461aa
Memnoc <smartdroidesign@gmail.com>
2019-01-19 17:28:31 +0000

    Functions Statements and Expressions

--- Language_Basics/script.js
+++ Language_Basics/script.js
@@ -252,4 +252,24 @@ switch (true) {
  * Functions Statements and Expressions
  **********************/
 
- 
+ // Function Expression -> Anything that produces an immediate result is an expression.
+ var whatDoYouDo = function(job, firstName) {
+    switch(job){
+        case 'teacher':
+            return firstName + ' teaches kids how to code';
+        case 'driver':
+            return firstName + ' drives a cab in Lisbon.'
+        case 'designer':
+        return firstName + ' designes beautiful websites.'
+        default:
+        return firstName + ' does something else.'
+    }
+ }
+
+ console.log(whatDoYouDo('teacher', 'John'));
+ console.log(whatDoYouDo('designer', 'Jane'));
+ console.log(whatDoYouDo('retired', 'Mark'));
+
+ // Statements perform actions, but they do not produce any immediate value.
+
+
