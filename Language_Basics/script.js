821ef0a84b936bd79317e9edfa4a355f28c93a7e
Memnoc <smartdroidesign@gmail.com>
2019-01-19 17:44:33 +0000

    Arrays

--- Language_Basics/script.js
+++ Language_Basics/script.js
@@ -272,4 +272,39 @@ switch (true) {
 
  // Statements perform actions, but they do not produce any immediate value.
 
+ /**********************
+ * Arrays
+ **********************/
 
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+// Initialize new array
+console.log(names[0]);
+console.log(names);
+console.log(names.length); // -> returns the number of elements
+
+// Mutate array data
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
+
+// Different data tyoes
+var john = ['John', 'Smith', 1990, 'teacher', false];
+
+// Arrays methods -> add -> push(add last), unshift(add first)
+john.push('blue');
+john.unshift('Mr.');
+console.log(john);
+
+// Arrays methods -> remove elements -> pop (remove last), shift(remove first)
+john.pop();
+john.pop();
+john.shift();
+console.log(john);
+
+// Arrays methods -> index -> indexOf -> returns the index of an element
+console.log(john.indexOf(23));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is not a designer' : 'John is a designer';
+console.log(isDesigner);
