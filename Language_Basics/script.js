236d7b0425d4182d0852e9d7ea72214849410598
Memnoc <smartdroidesign@gmail.com>
2019-01-21 09:06:55 +0000

    Loops and iterations

--- Language_Basics/script.js
+++ Language_Basics/script.js
@@ -367,3 +367,45 @@ var john = {
 john.calcAge();
 console.log(john);
 
+/**********************
+* Loops and iterations
+**********************/
+
+
+// For loop
+for (var i=1; i<=10; i++) {
+    console.log(i);
+}
+
+var john = ['John', 'Smith', 1990, 'teacher', false];
+for (var i=0; i<john.length; i++) {
+    console.log(john[i]);
+}
+
+
+// While loop
+var i = 0;
+while(i<john.length) {
+    console.log(john[i]);
+    i++;
+}
+
+// Continue and break statements
+var john = ['John', 'Smith', 1990, 'teacher', false];
+for (var i=0; i<john.length; i++) {
+    // If john's data type is different than a string, skip and continue
+    if (typeof john[i] !== 'string') continue; 
+    console.log(john[i]);
+}
+
+for (var i=0; i<john.length; i++) {
+    // If john's data type is different than a string, break out, do not continue;
+    if (typeof john[i] !== 'string') break; 
+    console.log(john[i]);
+}
+
+// Iterate array backward
+var john = ['John', 'Smith', 1990, 'teacher', false];
+for (var i=john.length-1; i>=0; i--) {
+    console.log(john[i]);
+}
