14b489e37b2e1513bed889565d4286c4003016c5
Memnoc <smartdroidesign@gmail.com>
2019-01-23 10:28:08 +0000

    execution stack and scope

--- How JS works/main.js
+++ How JS works/main.js
@@ -1,30 +1,50 @@
+// /**********************
+// * Hoisting
+// **********************/
+
+// // functions
+// calculateAge(1990); // -> function works even BEFORE it's declared
+
+// function calculateAge(year) {
+//     console.log(2016 - year);
+// }
+
+// retirement(1990); // function expression DOES NOT work -> variables are not available before declaration
+
+// var retirement = function(year){
+//     console.log(65 - (2016 - year));
+// }
+
+// // variables
+// console.log(age); // -> Undefined: variable is not available before declaration -> does not have a value
+// var age = 23;
+
+// function foo() {
+//     var age = 65;
+//     console.log(age);
+// }
+// foo();
+// console.log(age); // works, returns: undefined, 65, 23 in console
+
 /**********************
-* Hoisting
+* Execution Stack and Scope
 **********************/
 
-calculateAge(1990); // -> function works even BEFORE it's declared
+var a = 'Hello!';
+theFirst();
 
-function calculateAge(year) {
-    console.log(2016 - year);
+function theFirst() {
+    var b = 'Hi!';
+    theSecond();
 }
 
-retirement(1990); // function expression DOES NOT work -> variables are not available before declaration
-
-var retirement = function(year){
-    console.log(65 - (2016 - year));
+function theSecond() {
+    var c = 'Hey!';
+    theThird();
 }
 
-// variables
-
-console.log(age); // -> Undefined: variable is not available before declaration -> does not have a value
-var age = 23;
-
-function foo() {
-    var age = 65;
-    console.log(age);
+function theThird() {
+    var d = 'John';
+    console.log(c);
 }
-foo();
-console.log(age); // works, returns: undefined, 65, 23 in console
-
-
 
