3bfb9bb1faa656aebd86c8ac1671b1e0a4c77c93
Memnoc <smartdroidesign@gmail.com>
2019-01-23 09:26:05 +0000

    Hoisting - functions, function expressions, variables

---
+++ How JS works/main.js
@@ -0,0 +1,30 @@
+/**********************
+* Hoisting
+**********************/
+
+calculateAge(1990); // -> function works even BEFORE it's declared
+
+function calculateAge(year) {
+    console.log(2016 - year);
+}
+
+retirement(1990); // function expression DOES NOT work -> variables are not available before declaration
+
+var retirement = function(year){
+    console.log(65 - (2016 - year));
+}
+
+// variables
+
+console.log(age); // -> Undefined: variable is not available before declaration -> does not have a value
+var age = 23;
+
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age); // works, returns: undefined, 65, 23 in console
+
+
+
