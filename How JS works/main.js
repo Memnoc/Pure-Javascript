941224fd3f74b9a31d7d2846f2aa328dd23fd053
Memnoc <smartdroidesign@gmail.com>
2019-01-23 10:44:42 +0000

    "this" keyword and method borrowing

--- How JS works/main.js
+++ How JS works/main.js
@@ -1,30 +1,30 @@
-// /**********************
+//**********************
 // * Hoisting
-// **********************/
+//**********************/
 
-// // functions
-// calculateAge(1990); // -> function works even BEFORE it's declared
+// functions
+calculateAge(1990); // -> function works even BEFORE it's declared
 
-// function calculateAge(year) {
-//     console.log(2016 - year);
-// }
+function calculateAge(year) {
+    console.log(2016 - year);
+}
 
-// retirement(1990); // function expression DOES NOT work -> variables are not available before declaration
+retirement(1990); // function expression DOES NOT work -> variables are not available before declaration
 
-// var retirement = function(year){
-//     console.log(65 - (2016 - year));
-// }
+var retirement = function(year){
+    console.log(65 - (2016 - year));
+}
 
-// // variables
-// console.log(age); // -> Undefined: variable is not available before declaration -> does not have a value
-// var age = 23;
+// variables
+console.log(age); // -> Undefined: variable is not available before declaration -> does not have a value
+var age = 23;
 
-// function foo() {
-//     var age = 65;
-//     console.log(age);
-// }
-// foo();
-// console.log(age); // works, returns: undefined, 65, 23 in console
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age); // works, returns: undefined, 65, 23 in console
 
 /**********************
 * Execution Stack and Scope
@@ -48,3 +48,39 @@ function theThird() {
     console.log(c);
 }
 
+/**********************
+* The 'this' keyword
+**********************/
+
+console.log(this);
+calculateAge(1985);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+    console.log(this); // -> In this case, 'this' is the Window Object
+}
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function () {
+        console.log(this); // -> In this case, 'this' is the john Object
+        console.log(2016 - this.yearOfBirth);
+
+        function innerFunction() {
+            console.log(this); // -> In this case, 'this' is the Window Object - why? It's a REGULAR function, not a funciont expression
+        }
+        innerFunction();
+    }
+}
+john.calculateAge();
+
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
+
+// Method borrowing
+// Using john object method with mike object
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
