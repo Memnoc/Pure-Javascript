3103e22dadd6c3e32db7cc341a697c605589198d
Memnoc <smartdroidesign@gmail.com>
2019-02-09 17:41:50 +0000

    First Class Functions

--- Objects and Functions/script2.js
+++ Objects and Functions/script2.js
@@ -35,14 +35,14 @@ var john = {
 };
 
 // Function Constructor
-var Person = function(name, yearOfBirth, job) {
+var Person = function (name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
 }
 
 // Create the John object -> INSTANTIATION
-var john = new Person ('John', 1990, 'teacher');
+var john = new Person('John', 1990, 'teacher');
 console.log(john);
 
 /**************** How it works ****************/
@@ -54,24 +54,24 @@ console.log(john);
  * As a result, the parameters set in the Constructors can now be defined in the newly created object.
  */
 
- /**
-  * Function Constructors can be converted to class decalartion -> Much better IMHO
-  */
- class Warrior {
-     constructor(name, power, race) {
-         this.name = name;
-         this.power = power;
-         this.race = race;
-     }
- }
+/**
+ * Function Constructors can be converted to class decalartion -> Much better IMHO
+ */
+class Warrior {
+    constructor(name, power, race) {
+        this.name = name;
+        this.power = power;
+        this.race = race;
+    }
+}
 
- var goku = new Warrior('Goku', 'Kamehameha', 'Sayajin Goddo');
- console.log(goku);
- 
- /**************** Bringing INHERITANCE in ****************/
+var goku = new Warrior('Goku', 'Kamehameha', 'Sayajin Goddo');
+console.log(goku);
 
- class PersonTwo {
-     constructor(nameTwo, ageTwo, jobTwo) {
+/**************** Bringing INHERITANCE in ****************/
+
+class PersonTwo {
+    constructor(nameTwo, ageTwo, jobTwo) {
         this.nameTwo = nameTwo;
         this.ageTwo = ageTwo;
         this.jobTwo = jobTwo;
@@ -79,50 +79,50 @@ console.log(john);
         // this.calculateAge = function() {
         //     console.log(2019 - this.ageTwo);
         // }
-     }
- }
+    }
+}
 
- /**
-  * Each object can access the calculateAge() function from the constructor
-  * This is not DRY
-  * You can remedy adding the method to the Constructor's Prototype property
-  * PROTOTYPES ALLOW YOU TO EASILY DEFINE METHODS TO ALL INSTANCES OF A PARTICULAR OBJECT.
-  * ONCE THE METHOD IS APPLIED TO THE PROTOTYPE, IT'S ONLY STORED IN MEMORY ONCE,
-  * BUT EVERY INSTANCE OF THE OBJECT HAS ACCESS TO IT.
-  */
+/**
+ * Each object can access the calculateAge() function from the constructor
+ * This is not DRY
+ * You can remedy adding the method to the Constructor's Prototype property
+ * PROTOTYPES ALLOW YOU TO EASILY DEFINE METHODS TO ALL INSTANCES OF A PARTICULAR OBJECT.
+ * ONCE THE METHOD IS APPLIED TO THE PROTOTYPE, IT'S ONLY STORED IN MEMORY ONCE,
+ * BUT EVERY INSTANCE OF THE OBJECT HAS ACCESS TO IT.
+ */
 
-  /**
-   * Added the method to the Constructor's Prototype property
-   * Even though THE METHOD IS NOT IN THE CONSTRUCTOR ANYMORE, 
-   * WE CAN STILL USE IT.
-   */
- PersonTwo.prototype.calculateAge = function() {
+/**
+ * Added the method to the Constructor's Prototype property
+ * Even though THE METHOD IS NOT IN THE CONSTRUCTOR ANYMORE, 
+ * WE CAN STILL USE IT.
+ */
+PersonTwo.prototype.calculateAge = function () {
     console.log(2019 - this.ageTwo);
 }
 
 /**
  * Same can be done with properties. You can store them in the proto
  */
- PersonTwo.prototype.thirdJob = 'Support Agent';
+PersonTwo.prototype.thirdJob = 'Support Agent';
 
- var mike = new PersonTwo('Mark', 1984, 'Coder');
- var jane = new PersonTwo('Jane', 1974, 'Designer');
- var john = new PersonTwo('John', 1964, 'Plumber');
- mike.calculateAge();
- jane.calculateAge();
- john.calculateAge();
+var mike = new PersonTwo('Mark', 1984, 'Coder');
+var jane = new PersonTwo('Jane', 1974, 'Designer');
+var john = new PersonTwo('John', 1964, 'Plumber');
+mike.calculateAge();
+jane.calculateAge();
+john.calculateAge();
 
- console.log(mike.thirdJob);
+console.log(mike.thirdJob);
 
- /**
-  ********* Useful Properties to run in console to analyze objects *********
-  john.hasOwnProperty('jobTwo'); -> true (property owned by the object john).
-  john.hasOwnProperty('thirdJob'); -> false (not the property of john, but of the proto).
-  john instanceof Person -> false
-  john instanceof PersonTwo -> true 
-  */
+/**
+ ********* Useful Properties to run in console to analyze objects *********
+ john.hasOwnProperty('jobTwo'); -> true (property owned by the object john).
+ john.hasOwnProperty('thirdJob'); -> false (not the property of john, but of the proto).
+ john instanceof Person -> false
+ john instanceof PersonTwo -> true 
+ */
 
-/**************** OBJECTS.CREATE ****************/  
+/**************** OBJECTS.CREATE ****************/
 /**************** Objects inheriting from a prototype ****************/
 /**
  * FUNCTION CONSTRUCTOR VS OBJECT.CREATE
@@ -135,8 +135,8 @@ console.log(john);
  *    
 */
 var personProto = {
-    calculateAge: function() {
-        console.log(2016-this.yearOfBirth);
+    calculateAge: function () {
+        console.log(2016 - this.yearOfBirth);
     }
 };
 
@@ -145,13 +145,13 @@ john.name = 1990;
 john.job = 'teacher';
 
 var jane = Object.create(personProto, {
-    name: { value: 'Jane'},
-    yearOfBirth: {value: 1959},
-    job: {value: 'programmer'}
+    name: { value: 'Jane' },
+    yearOfBirth: { value: 1959 },
+    job: { value: 'programmer' }
 });
 
 
-/**************** PRIMITIVES VS OBJECTS ****************/ 
+/**************** PRIMITIVES VS OBJECTS ****************/
 /**
  * PRIMITVES Numbers, Strings, Booleans, Undefined, Null
  * OBJECTS Arrays, Functions, Objects, Dates, Wrappers for Numbers, Strings, and Booleans
@@ -163,36 +163,83 @@ var jane = Object.create(personProto, {
  * - VARIABLES CONTAINING OBJECTS, HOLD A REFERENCE TO THE PLACE IN MEMORY WHERE THE OBJECT SITS, NOT THE ACTUAL OBJECT ITSELF.
  */
 //PRIMITIVES
- var a = 23;
- var b = a;
- a = 46;
- console.log(a,b); //-> 46, 23 -> 
- // Each variable actually hold their own copy of the data
+var a = 23;
+var b = a;
+a = 46;
+console.log(a, b); //-> 46, 23 -> 
+// Each variable actually hold their own copy of the data
 
- // OBJECTS
- var obj1 = {
-     name: 'Chris',
-     age: 26
- };
- var obj2 = obj1;
- obj1.age = 30;
- console.log(obj1.age, obj2.age); //-> 30, 30 -> 
- // Variables changed because they do not actually contain the data
+// OBJECTS
+var obj1 = {
+    name: 'Chris',
+    age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age, obj2.age); //-> 30, 30 -> 
+// Variables changed because they do not actually contain the data
 
- // FUNCTIONS
- var age = 27;
- var obj = {
-     name: 'Jonas',
-     city: 'Dublin'
- };
+// FUNCTIONS
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Dublin'
+};
 
- function change(a,b) {
-     a = 30;
-     b.city = 'Madison';
- }
+function change(a, b) {
+    a = 30;
+    b.city = 'Madison';
+}
 
- change(age, obj);
- console.log(age, obj.city); // -> 27, "Madison"; ->
- // 27 did not change -> it's a primitve, actually contains the variable.
- // obj.city did change -> it's an object, only contains a reference pointing to the object.
+change(age, obj);
+console.log(age, obj.city); // -> 27, "Madison"; ->
+// 27 did not change -> it's a primitve, actually contains the variable.
+// obj.city did change -> it's an object, only contains a reference pointing to the object.
+
+/**************** FIRST CLASS FUNCTIONS ****************/
+/**************** Passing functions as arguments *******/
+/**
+ * A function is an instance of the Object type.
+ * A function behaves like any other object.
+ * A function can be store in a variable a.k.a FUNCTION EXPRESSION.
+ * A function can be passed as argument to another function a.k.a FIRST CLASS FUNCTIONS
+ * We can return a function from a function.
+ */
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+// Functions to use as fn functions -> a.k.a CALLBACK FUNCTIONS
+// 1
+function calculateAge(el) {
+    return 2019 - el;
+}
+// 2
+function isFullAge(el) {
+    return el >= 18;
+}
+
+// 3
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+
+}
+
+var ageResults = arrayCalc(years, calculateAge);
+var fullAgesResults = arrayCalc(ageResults, isFullAge);
+var avgHeartRate = arrayCalc(ageResults, maxHeartRate);
+console.log(ageResults);
+console.log(fullAgesResults);
+console.log(avgHeartRate);
 
